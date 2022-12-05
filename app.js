94fd7680b642c90b2e279307bcad7d6344643335
Jitendra Giriya <jitendragiriya@gmail.com>
2022-12-05 21:52:50 +0530

    package file update

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const fileupload = require('express-fileupload')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials:true,
     optionSuccessStatus: 200,
 }));
+
 app.use(cookieParser());
-app.use(fileupload());
-app.use(express.static('public'))
-app.use(express.static('files'))
+app.use(fileupload()); 
 //user api 
 app.use('/api', require('./routes/User/UserRoute'))     //user route
 app.use('/api', require('./routes/orderRoute'))         //order route
