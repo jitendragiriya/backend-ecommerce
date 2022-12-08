0a5ad2421a2d5e6c25a21b30e40882ccbeec4c34
Jitendra Giriya <jitendragiriya@gmail.com>
2022-12-08 13:05:19 +0530

    process.env.cors_url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: `${process.env.CORS_URL}`,
     credentials:true,
     optionSuccessStatus: 200,
 }));
@@ -21,7 +21,7 @@ app.use('/api', require('./routes/orderRoute'))         //order route
 app.use('/api', require('./routes/StripePayment'))         //payment route
 app.use('/api', require('./routes/Admin/productRoute'))     // product route
 
-app.get("*", (req, res)=>{
+app.get("/", (req, res)=>{
     res.send("API is running...");
 })
 
