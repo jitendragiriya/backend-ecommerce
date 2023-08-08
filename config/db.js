f84eb7330a3e826e96738b7519fb53631d6b2672
jitendra <jitendragiriya@gmail.com>
2023-08-08 10:43:12 +0530

    mongodb url added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = async ()=>{
-   await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false}).then(() => console.log("Database connected successfully.")).catch((err) => {console.log(err);
-    })
-}
+const connectDB = async () => {
+  await mongoose
+    .connect(
+      `mongodb+srv://jitendra_giriya:${process.env.MONGODB_PASSWORD}@cluster0.jr137.mongodb.net/MERN-stack-E-commerce?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+      }
+    )
+    .then(() => console.log("Database connected successfully."))
+    .catch((err) => {
+      console.log(err);
+    });
+};
 module.exports = connectDB;
-
